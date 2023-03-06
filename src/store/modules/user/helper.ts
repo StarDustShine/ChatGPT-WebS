5e57ddd8f64dd45fd246513d1aadd9690ae9d5a8
StarDustShine <108602877+StarDustShine@users.noreply.github.com>
2023-03-06 22:06:17 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'http://www.ntcor.net/wp-content/uploads/2023/03/ava.jpg',
+      name: '恩科NTCOR',
+      description: 'Based By <a href="https://www.ntcor.net" class="text-blue-500" target="_blank" >恩科随笔</a>',
     },
   }
 }
